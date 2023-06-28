5bb0c7aacc1ada69796b9d5d977e87cc21725229
FCA <test.test>
2023-06-28 18:46:41 +0900

    音を鳴らすだけのスクリプトを作成

--- Assets/Script/SoundTest.cs
+++ Assets/Script/SoundTest.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class SoundTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // スピーカー・CDプレイヤー
+    [SerializeField]
+    private AudioSource audioSource;
+    // 音源データ１
+    [SerializeField]
+    private AudioClip clip1;
+    // 音源データ２
+    [SerializeField]
+    private AudioClip clip2;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            // 再生したいclipを指定
+            audioSource.clip = clip1; 
+            // 再生
+            audioSource.Play();
+        }
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            //再生したいclipを指定
+            audioSource.clip = clip2; 
+            // 再生
+            audioSource.Play(); 
+        }
     }
 }
